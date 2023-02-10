9b92e6b96a4e224f3cb5bdb6b2e7b893790fde4d
Yaniv Gershon <gershon.yaniv@gmail.com>
2023-02-10 15:26:36 +0200

    check answer logic and UI

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ let username;
 let level;
 let data;
 let i = 0;
-let score;
+let score = 0;
+let correctAnswer;
+let input;
+let selected;
+let correct;
 
+function setProgress(currentScreen, screens) {
+    progress.value = (currentScreen / (screens + 1)) * 100;
+}
 
 $("form").submit(function (e) {
     e.preventDefault();
@@ -16,7 +23,7 @@ $("form").submit(function (e) {
     $("#setup").hide();
     $("#board").show();
     let url = `https://flashcards-5g44.onrender.com/username/${username}/level/${level}`;
-    console.log(url);
+
     $.ajax({
         async: false,
         url: url,
@@ -27,30 +34,61 @@ $("form").submit(function (e) {
     nextQuestion();
 });
 
-$("#next").click(function(){
-    if(true){
-
-    }else{
-
-    }
+$("#next").click(function () {
     nextQuestion();
+    $("#check").show();
+    $("#next").hide();
+
+});
+
+
+$("#check").click(function () {
+    $("#check").hide();
+    $("#next").show();
+
+    if (input == correctAnswer) {
+        score++;
+        selected.find(".fa-circle-check").show();
+        selected.css("background-color","lightgreen");
+        console.log("correct! score: " + score);
+    } else {
+        selected.find(".fa-circle-xmark").show();
+        selected.css("background-color","#ff000042");
+        correct.css("background-color","lightgreen");
+        correct.find(".fa-circle-check").show();
+        console.log("wrong! score: " + score);
+    }
+});
+
+$(".answer").on("click", function () {
+    $(".answer").css("background-color","#F0F8FF");
+    $(this).css("background-color", "#007bff66");   
+    input = $(this).find(".answer-text").html();
+    selected =  $(this);
 });
 
 function nextQuestion() {
-    $(".question").html(data[i]["arabic"]);
 
+    $(".answer").css("background-color", "#F0F8FF");
+    $(".fa-regular").hide();
+
+
+    $(".question").html(data[i]["arabic"]);
+    correctAnswer = data[i]["hebrew"];
     let answers = [];
-    answers.push(data[i]["hebrew"]);
+    answers.push(correctAnswer);
     data[i]["wrong answers"].forEach(el => {
         answers.push(el);
     });
 
     answers = shuffle(answers);
 
-
-
     $(".answer").each(function (j, el) {
-        $(el).html(answers[j]);
+        $(el).find(".answer-text").html(answers[j]);
+        if(answers[j] == correctAnswer){
+            correct = $(el);
+        }
+        
     });
 
     i++;
@@ -59,18 +97,20 @@ function nextQuestion() {
 
 function shuffle(array) {
     let currentIndex = array.length, randomIndex;
-
-    // While there remain elements to shuffle.
     while (currentIndex != 0) {
-
-        // Pick a remaining element.
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex--;
-
-        // And swap it with the current element.
         [array[currentIndex], array[randomIndex]] = [
             array[randomIndex], array[currentIndex]];
     }
-
     return array;
+}
+
+
+function updateAnswer(username, wordId, result) {
+    $.ajax({
+        url: `https://madrasa-flashcards.onrender.com/username/yanivg/${username}/${wordId}/result/${result}`,
+        type: 'PUT',
+        success: function (response) { }
+    });
 }
